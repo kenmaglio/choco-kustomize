ba134d036ad708d2f946f3fe520ae79557975fcb
Ken Maglio <kenmaglio@outlook.com>
2019-02-26 19:54:21 -0600

    refactor for new version 2.0.2

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -1,20 +1,43 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = join-path $toolsDir 'kustomize_2.0.1_windows_amd64.exe'
 
-$packageArgs = @{ 
+# release version of kustomize: https://github.com/kubernetes-sigs/kustomize/releases
+$version = '2.0.2'
+# pattern for exe name
+$exe_name = "kustomize_$($version)_windows_amd64.exe"
+# only 64bit url
+$url = "https://github.com/kubernetes-sigs/kustomize/releases/download/v$($version)/$($exe_name)"
+
+# use $ checksum [exe] -t=sha256
+$version_checksum = '84FC39886F4B25A01A84C675F4ED52FEB0405062879554E7289169035D1F73C0'
+$checksum_type = 'sha256'
+
+# destination for exe
+$fileLocation = join-path $toolsDir $exe_name
+
+$getArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath  = $fileLocation
+  url64bit      = $url
+  checksum64    = $checksum
+  checksumType64= $type
+}
+
+Get-ChocolateyWebFile @getArgs
+
+$packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'kustomize*'
   fileType      = 'exe'
   silentArgs    = ""
   validExitCodes= @(0)
-  url64bit      = "https://github.com/kubernetes-sigs/kustomize/releases/download/v2.0.1/kustomize_2.0.1_windows_amd64.exe"
-  checksum64    = 'C0CE46C0C81BB7EE4B13128A3152E17B73FDE586057AE8C85EDDFA3134D28371'
-  checksumType64= 'sha256'
+  file64          = $fileLocation
+  checksum64    = $checksum
+  checksumType64= $type
   destination   = $toolsDir
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
 $binargs = @{
   name = 'kustomize'
