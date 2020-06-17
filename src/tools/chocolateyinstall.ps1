014039bd823c03f0ec907439c97d665cd4c6b978
Curtis Deptuck <curtdept@me.com>
2020-06-17 12:26:37 -0600

    update to 3.5.4

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -2,28 +2,48 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # release version of kustomize: https://github.com/kubernetes-sigs/kustomize/releases
-$version = '2.0.3'
-# pattern for exe name
-$exe_name = "kustomize_$($version)_windows_amd64.exe"
+$version = '3.5.4'
+# pattern for archive name
+$base_name = "kustomize_v$($version)_windows_amd64"
+$zip_name = $base_name + ".tar.gz"
+$tar_name = $base_name + ".tar"
+$exe_name = "kustomize.exe"
+
 # only 64bit url
-$url = "https://github.com/kubernetes-sigs/kustomize/releases/download/v$($version)/$($exe_name)"
+$url = "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv$($version)/$($zip_name)"
 
 # use $ checksum [exe] -t=sha256
-$version_checksum = '7D8BE317F58A245065E1EF57356631DA81B0DBAB40F5F7CFBE65B55D8D484B29'
+$archive_checksum = '97ABF54FBEC1D9503080ABFAFAFA3E4FEAFE1C33309DFF1A12A2520C78BA7A6B'
+$exe_checksum = '99950430E077A49B04633967C3C9B76CBED80B8FCD1A3AEEA97D63A9CC0DFAE3'
 $checksum_type = 'sha256'
 
-# destination for exe
-$fileLocation = join-path $toolsDir $exe_name
+# destinations
+$zipLocation = join-path $toolsDir $zip_name
+$exeLocation = join-path $toolsDir $exe_name
 
 $getArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileFullPath  = $fileLocation
+  fileFullPath  = $zipLocation
   url64bit      = $url
-  checksum64    = $checksum
-  checksumType64= $type
+  checksum64    = $archive_checksum
+  checksumType64= $checksum_type
 }
 
 Get-ChocolateyWebFile @getArgs
+
+$unzipArgs = @{
+  fileFullPath = $zipLocation
+  destination = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzipArgs
+
+$unzip2Args = @{
+  fileFullPath = join-path $toolsDir $tar_name
+  destination = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzip2Args
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -31,9 +51,9 @@ $packageArgs = @{
   fileType      = 'exe'
   silentArgs    = ""
   validExitCodes= @(0)
-  file64        = $fileLocation
-  checksum64    = $checksum
-  checksumType64= $type
+  file64        = $exeLocation
+  checksum64    = $exe_checksum
+  checksumType64= $checksum_type
   destination   = $toolsDir
 }
 
@@ -41,7 +61,7 @@ Install-ChocolateyInstallPackage @packageArgs
 
 $binargs = @{
   name = 'kustomize'
-  path = $fileLocation
+  path = $exeLocation
 }
 
 Install-BinFile @binArgs
